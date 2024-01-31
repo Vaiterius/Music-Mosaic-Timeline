42958c9cc6ce18fd0a702797e22e88eb62dbc6de
Vaiterius <gerard.gandionco@gmail.com>
2024-01-31 03:27:21 -0800

    Integrate google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import ReactGA from "react-ga4";
+
 import App from "./components/App.tsx";
 import "./index.css";
+
+ReactGA.initialize("G-6ERW21J7W1");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
